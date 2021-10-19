13625769462d91e7bcd546b9bd8c0b39f655c70a
Kristian Lentino <kristian.lentino@dieffetech.it>
2021-10-19 17:50:19 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Watermark-FPDF
  
+Permette di creare facilmente un watermark in un file PDF.
+Esempio:
+
+ ```php
+        use Kristianlentino\WatermarkFpdf\WatermarkFpdi;
+        $fpdi = new WatermarkFpdi();
+		$pdfPath = __DIR__. DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'../media/lf_ebooks/1634655065/file.pdf';
+		$pdfPathSave = __DIR__. DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'../media/lf_ebooks/1634655065/file_prova.pdf';
+		$pageCount = $fpdi->setSourceFile($pdfPath);
+		for ($pageNo = 1; $pageNo <= $pageCount; $pageNo++) {
+
+			// import a page
+			$templateId = $fpdi->importPage($pageNo);
+
+			$fpdi->AddPage();
+
+			// use the imported page and adjust the page size
+			$fpdi->useTemplate($templateId);
+
+			// set alpha to semi-transparency
+			$fpdi->SetAlpha(0.3);
+			$fpdi->SetFont('Arial','B',80);
+			$fpdi->SetTextColor(188,190,190);
+			$fpdi->RotatedText(50,180,'Testo del watermark',45);
+			// set alpha to semi-transparency
+			$fpdi->SetAlpha(1);
+		}
+
+        /* 
+            I --> mostra il file nel browser,
+            D --> Download file 
+            F --> Salva il file in locale 
+            S --> Ritorna il file come stringa 
+        */
+		return $fpdi->Output($pdfPathSave,'I');
+```
+
+Queste funzioni sono state prese dai seguenti link:
+ - http://www.fpdf.org/en/script/script9.php
+ - http://www.fpdf.org/en/script/script2.php
+ - http://www.fpdf.org/en/script/script74.php
---
+++ src/WatermarkFpdi.php
@@ -0,0 +1,99 @@
+<?php
+namespace Kristianlentino\WatermarkFpdf;
+
+use setasign\Fpdi\Fpdi;
+
+class WatermarkFpdi extends Fpdi
+{
+	var $angle=0;
+	function RotatedText($x,$y,$txt,$angle)
+	{
+		//Text rotated around its origin
+		$this->Rotate($angle,$x,$y);
+		$this->Text($x,$y,$txt);
+		$this->Rotate(0);
+	}
+
+	function Rotate($angle,$x=-1,$y=-1)
+	{
+		if($x==-1)
+			$x=$this->x;
+		if($y==-1)
+			$y=$this->y;
+		if($this->angle!=0)
+			$this->_out('Q');
+		$this->angle=$angle;
+		if($angle!=0)
+		{
+			$angle*=M_PI/180;
+			$c=cos($angle);
+			$s=sin($angle);
+			$cx=$x*$this->k;
+			$cy=($this->h-$y)*$this->k;
+			$this->_out(sprintf('q %.5F %.5F %.5F %.5F %.2F %.2F cm 1 0 0 1 %.2F %.2F cm',$c,$s,-$s,$c,$cx,$cy,-$cx,-$cy));
+		}
+	}
+
+	protected $extgstates = array();
+
+	// alpha: real value from 0 (transparent) to 1 (opaque)
+	// bm:    blend mode, one of the following:
+	//          Normal, Multiply, Screen, Overlay, Darken, Lighten, ColorDodge, ColorBurn,
+	//          HardLight, SoftLight, Difference, Exclusion, Hue, Saturation, Color, Luminosity
+	function SetAlpha($alpha, $bm='Normal')
+	{
+		// set alpha for stroking (CA) and non-stroking (ca) operations
+		$gs = $this->AddExtGState(array('ca'=>$alpha, 'CA'=>$alpha, 'BM'=>'/'.$bm));
+		$this->SetExtGState($gs);
+	}
+
+	function AddExtGState($parms)
+	{
+		$n = count($this->extgstates)+1;
+		$this->extgstates[$n]['parms'] = $parms;
+		return $n;
+	}
+
+	function SetExtGState($gs)
+	{
+		$this->_out(sprintf('/GS%d gs', $gs));
+	}
+
+	function _enddoc()
+	{
+		if(!empty($this->extgstates) && $this->PDFVersion<'1.4')
+			$this->PDFVersion='1.4';
+		parent::_enddoc();
+	}
+
+	function _putextgstates()
+	{
+		for ($i = 1; $i <= count($this->extgstates); $i++)
+		{
+			$this->_newobj();
+			$this->extgstates[$i]['n'] = $this->n;
+			$this->_put('<</Type /ExtGState');
+			$parms = $this->extgstates[$i]['parms'];
+			$this->_put(sprintf('/ca %.3F', $parms['ca']));
+			$this->_put(sprintf('/CA %.3F', $parms['CA']));
+			$this->_put('/BM '.$parms['BM']);
+			$this->_put('>>');
+			$this->_put('endobj');
+		}
+	}
+
+	function _putresourcedict()
+	{
+		parent::_putresourcedict();
+		$this->_put('/ExtGState <<');
+		foreach($this->extgstates as $k=>$extgstate)
+			$this->_put('/GS'.$k.' '.$extgstate['n'].' 0 R');
+		$this->_put('>>');
+	}
+
+	function _putresources()
+	{
+		$this->_putextgstates();
+		parent::_putresources();
+	}
+}
